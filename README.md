395ec57c196307881f8fae57460dd8b04b4d59a3
ewelinastr <ewelinastruzynska@wp.pl>
2019-02-02 16:02:30 +0100

    adding guessinggame assignment files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unix Workbench project assignment by ewelinastr
+- The makefile was run at: **sob, 2 lut 2019, 16:01:13 CET**
+- There were **39** lines of code in guessinggame.sh
---
+++ guessinggame.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+#This program will continuously ask the user to guess the number of files in the current directory, until they guess the correct number. 
+#The user will be informed if their guess is too high or too low.
+#Once the user guesses the correct number of files in the current directory they should be congratulated.
+
+echo "Guess how many files are in the current directory?"
+read response
+
+game='on'
+
+function how_many_files_in_here {
+	ls | wc -l
+}
+
+while [[ $game = 'on' ]]
+do
+	if [[ $response =~ ^[0-9]+$ ]]
+	then
+		if [[ $response -eq $(how_many_files_in_here)  ]]
+		then
+			echo "Hoooray - You Guessed!"
+			let game='off'
+
+		elif [[ $response -gt $(how_many_files_in_here) ]]
+		then
+			echo "Try again - the number you provided is too high"
+			read response
+		elif [[ $response -lt $(how_many_files_in_here) ]]
+		then
+			echo "Try again - the number you provided is too low"
+			read response
+		fi
+	else
+		echo "Please give integer number"
+		read response
+	fi
+done
